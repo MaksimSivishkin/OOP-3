4993061de69266e84207c0863195d8e2c3130734
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-03-17 16:23:43 +0300

    ООП 3 Задание 1

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -6,18 +6,10 @@ public class Start {
         Point point1 = new Point(5,8);
         Point point2 = new Point(10,11);
         Point point3 = new Point(15,19);
-
-        Line line = new Line(point, point1);
-        Line line1 = new Line(point2, point3);
-        Line line2 = new Line(point1, point2);
+        Point3D point3D = new Point3D(point, 1);
 
 
-        System.out.println(line2);
-        point1.setX(0);
-        point1.setY(0);
-        point2.setX(3);
-        point2.setY(4);
-        System.out.println(line2);
-        System.out.println(line.getLine() + line1.getLine() + line2.getLine());
+        System.out.println(point3D);
+
     }
 }
