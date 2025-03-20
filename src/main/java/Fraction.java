4bafd4d8e2c2617c9d1629af41cfb48e60e9deea
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-03-20 17:49:29 +0300

    ООП 3 Задание 5

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,96 @@
+public class Fraction extends Number {
+    private int numerator;
+    private int denominator;
+
+
+    Fraction(int numerator){
+        this.numerator=numerator;
+        this.denominator=1;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        if(denominator<0){
+            throw new IllegalArgumentException("Denominator must be positive");
+        }
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if(denominator<0){
+            throw new IllegalArgumentException("Denominator must be positive");
+        }
+        this.denominator = denominator;
+    }
+    public Fraction sum(Fraction fraction) {
+        Fraction result = sum(fraction, this);
+        return result;
+    }
+
+    public static Fraction sum(Fraction a, Fraction b){
+        int cDenominator = Math1.nok(a.denominator, b.denominator);
+        int cNumerator = a.numerator*(cDenominator/a.denominator)+b.numerator*(cDenominator/ b.denominator);
+        Fraction c = new Fraction(cNumerator, cDenominator);
+        return c;
+    }
+    public static Fraction min(Fraction a, Fraction b){
+        int cDenominator = Math1.nok(a.denominator, b.denominator);
+        int cNumerator = a.numerator*(cDenominator/a.denominator)+b.numerator*(cDenominator/ b.denominator);
+        Fraction c = new Fraction(cNumerator, cDenominator);
+        return c;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s", numerator,denominator);
+    }
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+
+}
+
+class Math1 {
+    static int nok(int a, int b){
+        return a*b/nod(a,b);
+    }
+    static int nod(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return nod(b,a%b);
+    }
+    static double flInt(int a, int b){
+        double c=((double) a/ (double) b);
+        return c;
+    }
+}
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,28 +1,8 @@
 public class Start {
     public static void main(String[] args) {
-        Point point = new Point(1,3);
-        Point point1 = new Point(5,8);
-        Point point2 = new Point(10,11);
-        Point point3 = new Point(15,19); //Объявили точки
-        Line line = new Line(point,point1);
-        Line line1 = new Line(point1,point2);
-        Line line2 = new Line(point2, point3); //Объявили линии
-
-
-        ClosedPolyLine closedPolyLine = new ClosedPolyLine();
-        closedPolyLine.addPolyLine(line);
-        closedPolyLine.addPolyLine(line1);
-        closedPolyLine.addPolyLine(line2);
-        closedPolyLine.addPolyLine(new Line(point3,point));
-
-        PolyLine polyLine = new PolyLine();
-        polyLine.addPolyLine(line);
-        polyLine.addPolyLine(line1);
-        polyLine.addPolyLine(line2);
-        System.out.println(polyLine.sumPolyLine());
-        System.out.println(closedPolyLine.sumPolyLine());
-
-
+        Fraction fraction = new Fraction(1,3);
+        Fraction fraction1 = new Fraction(1,3);
+        Fraction fraction2 = new Fraction(2);
 
     }
 }
