7aa3eeb57ca33107a0c26fee5adea910d06edd8b
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-03-20 17:50:45 +0300

    ООП 3 Задание 6

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,6 +4,9 @@ public class Start {
         Fraction fraction1 = new Fraction(1,3);
         Fraction fraction2 = new Fraction(2);
 
+        System.out.println(2+Math1.flInt(3,5)+2.3);
+        System.out.println(3.6+Math1.flInt(49, 12)+3+Math1.flInt(3,2));
+        System.out.println(Math1.flInt(1,3)+1);
     }
 }
 interface Measurable{
